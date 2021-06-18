b4c0c0f6303a8e074b081c18cf319cfd6d15edcd
LucaArgentieri <Lucaargentieri-98@hotmail.it>
2021-06-18 10:39:05 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/App';
-import './index.scss'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
