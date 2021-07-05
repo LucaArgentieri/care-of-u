4bad84afd362fcd380c3b8388d8c39e93f354040
LucaArgentieri <lucaargentieri-98@hotmail.it>
2021-07-05 09:48:49 +0200

    update lazy components

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./style/app.scss";
@@ -7,39 +7,51 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Auth } from "./firebase/auth";
 import PrivateRoute from "./firebase/privateRoute";
+import Loading from "../app/components/loading/loading";
 
-import Landing from "./view/landing";
-import SignUp from "./view/signUp";
-import Login from "./view/login";
-import RetrievePassword from "./view/retrieve_password";
-import Error from "./view/error";
-
-import Application from "./view/application";
-import Dashboard from "./components/application/dashboard/dashboard";
-import Chat from "./components/application/chat/chat";
-import Analisi from "./components/application/analisi/analisi";
-import Impostazioni from "./components/application/impostazioni/settings";
+const Landing = lazy(() => import("./view/landing"));
+const Login = lazy(() => import("./view/login"));
+const SignUp = lazy(() => import("./view/signUp"));
+const RetrievePassword = lazy(() => import("./view/retrieve_password"));
+const Error = lazy(() => import("./view/error"));
+const Application = lazy(() => import("./view/application"));
+const Dashboard = lazy(() =>
+  import("./components/application/dashboard/dashboard")
+);
+const Chat = lazy(() => import("./components/application/chat/chat"));
+const Analisi = lazy(() => import("./components/application/analisi/analisi"));
+const Impostazioni = lazy(() =>
+  import("./components/application/impostazioni/settings")
+);
 
 function App() {
   return (
     <Auth>
       <Router>
         <CssBaseline />
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route
+              exact
+              path="/retrieve-password"
+              component={RetrievePassword}
+            />
 
-        <Switch>
-          <Route exact path="/" children={Landing} />
-          <Route exact path="/login" children={Login} />
-          <Route exact path="/signup" children={SignUp} />
-          <Route exact path="/retrieve-password" children={RetrievePassword} />
+            <PrivateRoute component={Application} path="/application" exact />
+            <PrivateRoute component={Dashboard} path="/application/dashboard" />
+            <PrivateRoute component={Chat} path="/application/chat" />
+            <PrivateRoute component={Analisi} path="/application/analysis" />
+            <PrivateRoute
+              component={Impostazioni}
+              path="/application/settings"
+            />
 
-          <PrivateRoute component={Application} path="/application" exact />
-          <PrivateRoute component={Dashboard} path="/application/dashboard" />
-          <PrivateRoute component={Chat} path="/application/chat" />
-          <PrivateRoute component={Analisi} path="/application/analysis" />
-          <PrivateRoute component={Impostazioni} path="/application/settings" />
-
-          <Route path="*" children={Error} />
-        </Switch>
+            <Route path="*" component={Error} />
+          </Switch>
+        </Suspense>
       </Router>
     </Auth>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import App from "./app/App";
 import "./index.scss";
+import Loading from "./app/components/loading/loading";
+
+const App = React.lazy(() => import("./app/App"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
